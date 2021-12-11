36b424ace6b539a9d4daee8e1af295d9c4a60598
Wen Jiang <wjiang@gmail.com>
2021-12-11 17:32:48 -0500

    auto-convert cloud drive preview link to direct download link

--- procart.py
+++ procart.py
@@ -72,6 +72,7 @@ def main():
             url_default = "https://files.rcsb.org/download/7MKH.pdb"
             help = "An online url (http:// or ftp://) or a local file path (/path/to/your/structure.pdb)"
             url = st.text_input(label="Input the url of a PDB file:", value=url_default, help=help, key="url")
+            url = get_direct_url(url)    # convert cloud drive indirect url to direct url
             with st.spinner(f'Downloading {url.strip()}'):
                 pdb = get_model_from_url(url.strip())
         elif input_mode == 2:
@@ -528,11 +529,11 @@ def get_model_from_pdb(pdb_id):
     model = atomium.fetch(pdb_id)
     return model
 
-@st.experimental_singleton(show_spinner=False)
+@st.experimental_singleton(show_spinner=False, suppress_st_warning=True)
 def get_model_from_url(url):
     ds = np.DataSource(None)
     if not ds.exists(url):
-        st.error(f"ERROR: {url} does not exist")
+        st.error(f"ERROR: {url} could not be downloaded. If this url points to a cloud drive file, make sure the link is a direct download link instead of a link for preview")
         st.stop()
     with ds.open(url) as fp:
         model = get_model_from_file(fp.name)
@@ -557,6 +558,28 @@ def get_model_from_uploaded_file(fileobj):
     with tempfile.NamedTemporaryFile(suffix=suffix) as temp:
         temp.write(fileobj.read())
         return get_model_from_file(temp.name)
+
+def get_direct_url(url):
+    import re
+    if url.startswith("https://drive.google.com/file/d/"):
+        hash = url.split("/")[5]
+        return f"https://drive.google.com/uc?export=download&id={hash}"
+    elif url.startswith("https://app.box.com/s/"):
+        hash = url.split("/")[-1]
+        return f"https://app.box.com/shared/static/{hash}"
+    elif url.startswith("https://www.dropbox.com"):
+        if url.find("dl=1")!=-1: return url
+        elif url.find("dl=0")!=-1: return url.replace("dl=0", "dl=1")
+        else: return url+"?dl=1"
+    elif url.find("sharepoint.com")!=-1 and url.find("guestaccess.aspx")!=-1:
+        return url.replace("guestaccess.aspx", "download.aspx")
+    elif url.startswith("https://1drv.ms"):
+        import base64
+        data_bytes64 = base64.b64encode(bytes(url, 'utf-8'))
+        data_bytes64_String = data_bytes64.decode('utf-8').replace('/','_').replace('+','-').rstrip("=")
+        return f"https://api.onedrive.com/v1.0/shares/u!{data_bytes64_String}/root/content"
+    else:
+        return url
 
 @st.cache(persist=True, show_spinner=False)
 def setup_anonymous_usage_tracking():
